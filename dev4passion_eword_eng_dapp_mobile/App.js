166fea209084dad509e9762302459e91651cf42d
System Administrator <root@bartoszs-MacBook-Pro.local>
2024-01-04 10:20:54 +0100

    dev4passion_eword_eng_dapp_mobile-ios-bottomtabbar

--- dev4passion_eword_eng_dapp_mobile/App.js
+++ dev4passion_eword_eng_dapp_mobile/App.js
@@ -6,7 +6,11 @@
  * @flow strict-local
  */
 
-import React from 'react';
+//import React from 'react';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -16,6 +20,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Dimensions
 } from 'react-native';
 
 import {
@@ -25,6 +30,88 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+const { width, height } = Dimensions.get("window");
+
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+const SettingsScreen = () => {
+  return (
+    <Text>Settings</Text>
+  )
+}
+
+const LearnEngWords = () => {
+  return (
+    <Text>Learn</Text>
+  )
+}
+
+const AddWords = () => {
+  return (
+    <Text>AddWord</Text>
+  )
+}
+
+const EngWordssss = () => {
+  return (
+    <Text>Learn</Text>
+  )
+}
+
+const VideoPlayer = () => {
+  return (
+    <Text>Learn</Text>
+  )
+}
+
+const Check = () => {
+  return (
+    <Text>Learn</Text>
+  )
+}
+
+const BottomTabNavigator = () => {
+  return (
+    <View style={{
+      width,
+      height,
+    }}>
+
+
+      <Tab.Navigator>
+        {/* <Tab.Screen name='Home' component={MainScreen} /> */}
+
+        {/* <Tab.Screen name='Home' component={DrawerNavigator} /> */}
+
+        {/* <Tab.Screen name='Learn' component={LearnnnnScreen} options={{ headerShown: false }}/> */}
+        <Tab.Screen name='Learn' component={LearnEngWords} options={{ headerShown: false }} />
+        <Tab.Screen name='Add Word' component={AddWords} options={{ headerShown: false }} />
+
+        <Tab.Screen name='Check' component={Check} options={{ headerShown: false }} />
+
+        {/* <Tab.Screen name='Words' component={WordssScreen} /> */}
+        {/* <Tab.Screen name='Words' component={EngWords} options={{ headerShown: false }}/> */}
+        <Tab.Screen name='Words' component={EngWordssss} options={{ headerShown: false }} />
+        {/* <Tab.Screen name='Words' component={EngWordss} options={{ headerShown: false }}/> */}
+
+        <Tab.Screen name="videoplayer" component={VideoPlayer} />
+
+        {/* <Tab.Screen name='Contact' component={ContactScreen} /> */}
+      </Tab.Navigator>
+    </View>
+  )
+}
+
+
+
+
+//}
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -60,33 +147,57 @@ const App: () => Node = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
+    <NavigationContainer >
+
+    {/* <SafeAreaView style={backgroundStyle}> */}
+
+      {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} /> */}
+
+      {/* <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+        style={backgroundStyle}> */}
+
+        {/* <Header /> */}
+     
+        {/* <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
+          }}> */}
+
+
+          {/* <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
+          </Section> */}
+          {/* <Section title="See Your Changes">
             <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
+          </Section> */}
+          {/* <Section title="Debug">
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          <LearnMoreLinks /> */}
+          
+          {/* <NavigationContainer> */}
+
+
+          {/* <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator> */}
+          
+          
+          {/* </NavigationContainer> */}
+
+           
+        {/* </View>           
+      </ScrollView>         
+    </SafeAreaView> */}
+
+      <BottomTabNavigator />    
+    </NavigationContainer >
   );
 };
 
